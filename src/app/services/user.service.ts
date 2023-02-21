400289082c803cf8c214e6a8c17cf7ec18b3a315
Piyush7034 <piyushshukla2100@gmail.com>
2023-02-21 13:14:45 +0530

    Added login validations

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Login, SignUp } from '../data-type';
 
@@ -7,7 +7,7 @@ import { Login, SignUp } from '../data-type';
   providedIn: 'root'
 })
 export class UserService {
-
+  invalidUserAuth = new EventEmitter<boolean>(false);
   constructor(private http: HttpClient, private router: Router) { }
 
   userSignUp(user: SignUp) {
@@ -33,9 +33,12 @@ export class UserService {
         { observe: 'response' }
       )
       .subscribe((result) => {
-        if(result && result.body) {
+        if(result && result.body?.length) {
           localStorage.setItem('user', JSON.stringify(result.body[0]));
           this.router.navigate(['/']);
+          this.invalidUserAuth.emit(false);
+        } else {
+          this.invalidUserAuth.emit(true);
         }
       });
   }
