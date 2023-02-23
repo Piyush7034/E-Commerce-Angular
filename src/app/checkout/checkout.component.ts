035f4d45917d693784f04d6192d3e65bea42876f
Piyush7034 <piyushshukla2100@gmail.com>
2023-02-23 12:44:10 +0530

    Cancel order added

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Order } from '../data-type';
+import { Cart, Order } from '../data-type';
 import { ProductService } from '../services/product.service';
 
 @Component({
@@ -10,6 +10,8 @@ import { ProductService } from '../services/product.service';
 })
 export class CheckoutComponent implements OnInit {
   totalPrice: number | undefined;
+  cartData: Cart[] | undefined;
+  orderMessage: string | undefined;
   constructor(private product: ProductService, private router: Router) {  }
 
   ngOnInit(): void {
@@ -17,6 +19,7 @@ export class CheckoutComponent implements OnInit {
       .currentCart()
       .subscribe((result) => {
         let price = 0;
+        this.cartData = result;
         result.forEach((item) => {
           if(item.quantity) {
             price = price + (+item.price * +item.quantity);
@@ -34,12 +37,24 @@ export class CheckoutComponent implements OnInit {
       let orderData: Order = {
         ...data,
         totalPrice: this.totalPrice,
-        userId
+        userId,
+        id: undefined
       }
+
+      this.cartData?.forEach((item) => {
+        setTimeout(() => {
+          item.id && this.product.deleteCartItems(item.id);
+        }, 700);
+      });
+
       this.product
         .orderNow(orderData)
         .subscribe((result) => {
-          this.router.navigate(['/my-orders']);
+          this.orderMessage = "Your order has been placed!"
+          setTimeout(() => {
+            this.router.navigate(['/my-orders']);
+            this.orderMessage = undefined;
+          }, 4000);
         });
     }
   }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -125,4 +125,29 @@ export class ProductService {
     return this.http
             .post(`http://localhost:3000/orders`, data);
   }
+
+  orderList() {
+    let userStore = localStorage.getItem('user');
+    let userData = userStore && JSON.parse(userStore);
+    return this.http
+            .get<Order[]>(`http://localhost:3000/orders?userId=` + userData.Id);
+  }
+
+  deleteCartItems(cartId: number) {
+    return this.http
+            .delete(
+              `http://localhost:3000/cart/` + cartId,
+              { observe: 'response' }
+            )
+            .subscribe((result) => {
+              if(result) {
+                this.cartData.emit([]);
+              }
+            });
+  }
+
+  cancelOrder(orderId: number) {
+    return this.http
+            .delete(`http://localhost:3000/orders/` + orderId);
+  }
 }
